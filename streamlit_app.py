3fc9061cbd59708bbbbe0764f0a4a63305433ccb
Divy Parekh <128831707+Divy-Parekh@users.noreply.github.com>
2024-11-12 21:19:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('💹 marketing Campaign Analysis')
+st.title('📈📊Marketing Campaign Analysis')
 
-st.write('This is calculates impact of Marketing Campaign Analysis on sales and customer behaviour.')
+st.info('This app calculates impact of Marketing Campaign Analysis on sales and customer behaviour.')
