ad6f91c858d2053dc3f05356c0dd8f0c23e65cfb
Divy Parekh <128831707+Divy-Parekh@users.noreply.github.com>
2024-11-12 21:49:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+import pandas as pd
 st.title('📈Marketing Campaign Analysis')
 
 st.info('This app calculates impact of Marketing Campaign Analysis on sales and customer behaviour.')
+
+df = pd.read_csv('https://raw.githubusercontent.com/Divy-Parekh/mlproject/refs/heads/master/marketing_campaign_dataset.csv')
+df
