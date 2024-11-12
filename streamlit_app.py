05043bd6ed0c7c0c0da5b044210ad38a4dc792b0
Divy Parekh <128831707+Divy-Parekh@users.noreply.github.com>
2024-11-12 21:59:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,17 @@ st.title('📈Marketing Campaign Analysis')
 
 st.info('This app calculates impact of Marketing Campaign Analysis on sales and customer behaviour.')
 
-df = pd.read_csv('https://raw.githubusercontent.com/Divy-Parekh/mlproject/refs/heads/master/marketing_campaign_dataset.csv')
-df
+
+with st.expander('Data'):
+  st.write('**Raw Data')
+  df = pd.read_csv('https://raw.githubusercontent.com/Divy-Parekh/mlproject/refs/heads/master/marketing_campaign_dataset.csv')
+  df
+
+
+with st.expander('Data Visualization'):
+  avg_roi_by_campaign_type = df.groupby(['Campaign_Type'])['ROI'].mean()
+  st.bar_chart(avg_roi_by_campaign_type,x='Campaign Type',y='Return of Investment')
+  
+
+
+
